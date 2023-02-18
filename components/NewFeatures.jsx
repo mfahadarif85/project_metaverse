f705e5775ac1725685f073d10336b1213490f61d
mfahadarif85 <mfahadarif@gmail.com>
2023-02-18 13:23:49 +0500

    get started and features section

--- components/NewFeatures.jsx
+++ components/NewFeatures.jsx
@@ -1,6 +1,21 @@
-const NewFeatures = () => (
-  <div>
-    New Features
+import Image from "next/image";
+import styles from "../styles";
+
+const NewFeatures = ({ imgUrl, title, subtitle }) => (
+  <div className='flex-1 flex flex-col sm:max-w-[250px] min-w-[210px]'>
+    <div
+      className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323F5D]`}
+    >
+      <picture>
+        <img src={imgUrl} alt='icon' className='w-full h-full object-contain' />
+      </picture>
+    </div>
+    <h1 className='mt-[26px] font-bold text-[24px] leading-[30.24px] text-white'>
+      {title}
+    </h1>
+    <p className='flex-1 mt-[16px] font-normal text-[18px] text-[#B0B0B0] leading-[32.4px]'>
+      {subtitle}
+    </p>
   </div>
 );
 
